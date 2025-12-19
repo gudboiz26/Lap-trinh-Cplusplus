34430f33ab4117f20b61d713ca6af15a55dbdba1
gudboiz26 <vtuankiet65@gmail.com>
2025-12-19 22:44:22 +0700

    vidu1-1

---
+++ vidu1.cpp
@@ -0,0 +1,16 @@
+/* Chương trình này in ra một thông điệp văn bản trên màn hình.
+Chương trình bao gồm hai hàm có tên là
+PrintMessage và main. */
+#include <iostream>
+using namespace std;
+// Hàm PrintMessage()
+void PrintMessage()
+{
+    cout << "Getting Started" << endl;
+}
+
+// Hàm main.
+int main()
+{
+    PrintMessage(); // gọi hàm
+}
